622aac29e5a7e59a129f9522b736e04843918760
Tanya Goyal <tanya.goyal@tothenew.com>
2025-12-02 12:26:59 +0530

    task4 week1

---
+++ week1-assignment/task4.py
@@ -0,0 +1,15 @@
+s = "hello"
+
+print(s)
+
+print(s.upper())
+
+replaced_s = s.replace('e', 'a')
+print(replaced_s)
+
+starts = s.startswith('he')
+print(starts)
+
+ends = s.endswith('lo')
+print(ends)
+
