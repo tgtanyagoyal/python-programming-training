8f2f110a102bad77e86aea26d4bf49b073fd0a2f
Tanya Goyal <tanya.goyal@tothenew.com>
2025-12-02 12:19:50 +0530

    task1 week1

---
+++ week1-assignment/task1.py
@@ -0,0 +1,17 @@
+x = 3.75
+y = int(x)
+print(y)           # Convert to an integer
+
+x = "123"
+y = float(x)
+print(y)          # convert to float
+
+x = 0
+y = bool(x)
+print(y)          # convert to boolean
+
+x = False
+y = str(x)
+print(y)          # convert to string
+
+
