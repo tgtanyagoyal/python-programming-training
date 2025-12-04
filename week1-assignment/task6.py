8b4c28562923044c0d91ba09c57697c7aa3a9e2f
Tanya Goyal <tanya.goyal@tothenew.com>
2025-12-04 12:44:23 +0530

    week1 input output

---
+++ week1-assignment/task6.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+sum = num1 + num2
+diff = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print("Sum = ",sum)
+print("Difference = ",diff)
+print("multiplication = ",mul)
+print("Division = ",div)
+
